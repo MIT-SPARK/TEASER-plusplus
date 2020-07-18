bac401fffbde5298fb696ef6688d9462162e9558
jingnanshi <jnshi@mit.edu>
2020-07-18 15:35:36 -0400

    add certification test skeleton

---
+++ test/teaser/certification-test.cc
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2020, Massachusetts Institute of Technology,
+ * Cambridge, MA 02139
+ * All Rights Reserved
+ * Authors: Jingnan Shi, et al. (see THANKS for the full author list)
+ * See LICENSE for the license information
+ */
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include <iostream>
+#include <random>
+
+#include <Eigen/Core>
+
+#include "teaser/certification.h"
+
+TEST(DRSCertifierTest, GetOmega1) {
+  ASSERT_TRUE(false);
+}
+
+TEST(DRSCertifierTest, GetBlockDiagOmega) {
+  ASSERT_TRUE(false);
+}
+
+TEST(DRSCertifierTest, GetQCost) {
+  ASSERT_TRUE(false);
+}
+
+TEST(DRSCertifierTest, GetLambdaGuess) {
+  ASSERT_TRUE(false);
+}
+
+TEST(DRSCertifierTest, GetLinearProjection) {
+  ASSERT_TRUE(false);
+}
+
