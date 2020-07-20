977e2280cabe1b66e23ae5b476e5de8de1f670e3
jingnanshi <jnshi@mit.edu>
2020-07-20 12:24:51 -0400

    Add Q cost test
    Use the correct noise bound for the other tests

--- test/teaser/certification-test.cc
+++ test/teaser/certification-test.cc
@@ -19,7 +19,7 @@
 
 TEST(DRSCertifierTest, GetOmega1) {
   {
-    // a small case
+    // Case 1: N=10
     // read in expected data
     std::ifstream omega_source_file("./data/certification_test/case_1/omega.csv");
     std::ifstream q_source_file("./data/certification_test/case_1/q.csv");
@@ -27,7 +27,7 @@ TEST(DRSCertifierTest, GetOmega1) {
     auto expected_output =
         teaser::test::readFileToEigenFixedMatrix<double, 4, 4>(omega_source_file);
     double cbar2 = 1;
-    double noise_bound = 0.0021;
+    double noise_bound = 4.594291399787397e-02;
 
     // construct the certifier
     teaser::DRSCertifier certifier(noise_bound, cbar2);
@@ -46,36 +46,69 @@ TEST(DRSCertifierTest, GetOmega1) {
 }
 
 TEST(DRSCertifierTest, GetBlockDiagOmega) {
-  // a small example
-  // read in expected data
-  std::ifstream bdomega_source_file("./data/certification_test/case_1/block_diag_omega.csv");
-  std::ifstream q_source_file("./data/certification_test/case_1/q.csv");
-  auto q_mat = teaser::test::readFileToEigenFixedMatrix<double, 4, 1>(q_source_file);
-  auto expected_output =
-      teaser::test::readFileToEigenMatrix<double, Eigen::Dynamic, Eigen::Dynamic>(
-          bdomega_source_file);
-  double cbar2 = 1;
-  double noise_bound = 0.0021;
-  int N = 10;
-  int Npm = 44;
+  {
+    // Case 1: N=10
+    // read in expected data
+    std::ifstream bdomega_source_file("./data/certification_test/case_1/block_diag_omega.csv");
+    std::ifstream q_source_file("./data/certification_test/case_1/q.csv");
+    auto q_mat = teaser::test::readFileToEigenFixedMatrix<double, 4, 1>(q_source_file);
+    auto expected_output =
+        teaser::test::readFileToEigenMatrix<double, Eigen::Dynamic, Eigen::Dynamic>(
+            bdomega_source_file);
+    double cbar2 = 1;
+    double noise_bound = 4.594291399787397e-02;
+    int N = 10;
+    int Npm = 44;
 
-  // construct the certifier
-  teaser::DRSCertifier certifier(noise_bound, cbar2);
-  Eigen::Quaternion<double> q(q_mat(0), q_mat(1), q_mat(2), q_mat(3));
+    // construct the certifier
+    teaser::DRSCertifier certifier(noise_bound, cbar2);
+    Eigen::Quaternion<double> q(q_mat(0), q_mat(1), q_mat(2), q_mat(3));
 
-  // perform the computation
-  Eigen::MatrixXd actual_output;
-  certifier.getBlockDiagOmega(Npm, q, &actual_output);
-  if (!actual_output.isApprox(expected_output)) {
-    std::cout << "Actual output: " << std::endl;
-    std::cout << actual_output << std::endl;
-    std::cout << "Expected output: " << std::endl;
-    std::cout << expected_output << std::endl;
+    // perform the computation
+    Eigen::MatrixXd actual_output;
+    certifier.getBlockDiagOmega(Npm, q, &actual_output);
+    if (!actual_output.isApprox(expected_output)) {
+      std::cout << "Actual output: " << std::endl;
+      std::cout << actual_output << std::endl;
+      std::cout << "Expected output: " << std::endl;
+      std::cout << expected_output << std::endl;
+    }
+    ASSERT_TRUE(actual_output.isApprox(expected_output));
   }
-  ASSERT_TRUE(actual_output.isApprox(expected_output));
 }
 
-TEST(DRSCertifierTest, GetQCost) { ASSERT_TRUE(false); }
+TEST(DRSCertifierTest, GetQCost) {
+  {
+    // Case 1: N=10
+    // load parameters
+    std::ifstream v1_source_file("./data/certification_test/case_1/v1.csv");
+    std::ifstream v2_source_file("./data/certification_test/case_1/v2.csv");
+    std::ifstream Q_cost_source_file("./data/certification_test/case_1/Q_cost.csv");
+    auto v1 = teaser::test::readFileToEigenMatrix<double, 3, Eigen::Dynamic>(v1_source_file);
+    auto v2 = teaser::test::readFileToEigenMatrix<double, 3, Eigen::Dynamic>(v2_source_file);
+    auto expected_output =
+        teaser::test::readFileToEigenMatrix<double, Eigen::Dynamic, Eigen::Dynamic>(
+            Q_cost_source_file);
+    double cbar2 = 1;
+    double noise_bound = 4.594291399787397e-02;
+    int N = 10;
+    int Npm = 44;
+
+    // construct the certifier
+    teaser::DRSCertifier certifier(noise_bound, cbar2);
+
+    // perform the computation
+    Eigen::MatrixXd actual_output;
+    certifier.getQCost(v1, v2, &actual_output);
+    if (!actual_output.isApprox(expected_output)) {
+      std::cout << "Actual output: " << std::endl;
+      std::cout << actual_output << std::endl;
+      std::cout << "Expected output: " << std::endl;
+      std::cout << expected_output << std::endl;
+    }
+    ASSERT_TRUE(actual_output.isApprox(expected_output));
+  }
+}
 
 TEST(DRSCertifierTest, GetLambdaGuess) { ASSERT_TRUE(false); }
 
